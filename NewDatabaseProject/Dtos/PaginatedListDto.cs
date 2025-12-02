c0860d4c6d42484effe30ca8c30c8717df6fd1f3
MichaelRdot <mruss62@lsu.edu>
2025-12-02 13:03:29 -0600

    Made PaginatedListDto.cs and Made the json files readable to it.

---
+++ NewDatabaseProject/Dtos/PaginatedListDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NewDatabaseProject.Dtos;
+
+public class PaginatedListDto<T>
+{
+    [JsonPropertyName("Object")] public required string ObjectType { get; set; }
+    [JsonPropertyName("Data")] public required T Data { get; set; }
+}
